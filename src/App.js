231ddb5716efdae144fe6551a28f893d69bd2d20
NG <msnicoleta@outlook.com>
2023-03-14 14:49:18 +0000

    Z-index on the form background

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-// import Clarifai from 'clarifai';
 import ParticlesOptions from './components/ParticlesOptions';
+// import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
-import './App.css';
 import Tachyons from 'tachyons';
+import './App.css';
 
-
-
+//Api Key  here from https://clarifai.com/
 // const app = new Clarifai.App({
 //   apiKey: 'b932a3c738a4e18a7eec11b756509e5'
 // });
@@ -37,7 +36,8 @@ class App extends Component {
         <Rank />
         <ImageLinkForm
           onInputChange={this.onInputChange}
-          onButtonSubmit={this.onButtonSubmit} />
+          onButtonSubmit={this.onButtonSubmit}
+           />
         {/* <FaceRecognition/> */}
       </div>
     );
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -10,9 +10,12 @@ const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
             </p>
             <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
-                    <input className=' f4 pa2 w-70 center' type='tex' onChange={onInputChange} />
+                    <input className=' f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
+                    &nbsp;
+                    &nbsp;
                     <button
-                        className='w-30 grow f4 link ph3 pv2 dib white bg-gold br3 ba b--white-025'
+                        className='w-30 grow f4 link ph3 pv2 dib white bg-gold b
+                        r3 ba b--white-025'
                         onClick={onButtonSubmit}
                     > Detect</button>
                 </div>
