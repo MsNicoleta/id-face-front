42c64b39cf574b3e49dd006030f343d4f91a7508
NG <msnicoleta@outlook.com>
2023-03-20 22:39:25 +0000

    practice

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
-const server = http.createServer (()=>{
-    console.log ('Thanks for the requesr')
+const server = http.createServer ((request, response)=>{
+    console.log ('headers', request.headers)
+    console.log ('method', request.method)
+    console.log ('url', request.url)
+
+
+
+    response.setHeader('Content-Type', 'aplication/json');
+    response.end('<h1>Hello</h1>');
+    console.log ('Thanks for the request')
 })
 
 server.listen (3000);
